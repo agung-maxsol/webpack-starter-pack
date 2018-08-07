9719b4c38903d17012453d365dd36902c6a873f2
Gs Mahardika <gs.mahardika@gmail.com>
2018-08-07 14:23:27 +0800

    Try old and new arrow function

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,19 @@
 import _ from 'lodash';
 import '../css/app.css';
 
+// ES6 function style
+let loader = () => {
+    console.log('This is loaded...');
+}
+
+// Old function style
 function component() {
     let element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'Webpack...'], ' ');
     element.className = 'append-text';
+
+    loader();
 
     return element;
 }
